374a03f6f285ca2e263e67860ad56790e4dfd2ea
phirumsim <simphirum@gmail.com>
2018-01-26 09:42:45 +0700

    update to more 01-2018 files

---
+++ 01-2018 Js/1sample.js
@@ -0,0 +1,10 @@
+let start=moment('2018-01-01').toDate();
+let end=moment('2018-01-30').toDate();
+
+let barcodes=
+
+
+barcodes.forEach(function(bar){
+    let customer=db.wb_customer.findOne({dpc:bar.dpc});
+    db.wb_meterReadingJournalDetails.update({customerId:customer._id,newReadingDate:{$gt:start,$lte:end}},{$set:{subBarcode:bar.barcode}});
+});
